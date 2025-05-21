7a9940d95cc13b2385ff3ade007bb19b1f7bccde
Johnny3356 <paggy1999955@gmail.com>
2025-05-21 20:47:59 +0800

    test

--- src/fileio.cpp
+++ src/fileio.cpp
@@ -30,3 +30,22 @@ void def_file_in(){
 
     ifile.close();
 }
+
+void nets_file_in(){
+    ifstream ifile;
+    ifile.open(input_file);
+
+    if (!ifile.is_open()){
+        cout << "Error opening file: " << input_file << endl;
+        exit(1);
+    }
+    
+    string word = "",word2,word3,word4;
+    int x1,y1,x2,y2,width;
+
+    while (word!="NETS"){   // file in NETS
+        ifile>>word; 
+    }
+
+    ifile.close();
+}
