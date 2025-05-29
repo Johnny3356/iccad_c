e86c36ad48f42f130aba2fc27fc6758e6cae9acc
Johnny3356 <paggy1999955@gmail.com>
2025-05-30 01:52:08 +0800

    改名字

--- src/class_def.h
+++ src/class_def.h
@@ -2,7 +2,7 @@
 #define _CLASSDEF_
 #include "bits/stdc++.h"
 
-#include "component_def.h"
+#include "macro_def.h"
 #include "die_def.h"
 #include "row_def.h"
 #include "track_def.h"
@@ -22,30 +22,6 @@ enum DIRECTION{
     FE,
     FW
 };
-
-// die_def.h
-class die;
-
-// component_def.h
-class component;
-
-// net_def.h
-class net;
-
-// net_def.h
-class pin;
-
-// row_def.h
-class row;
-class site;
-
-//track_def.h
-class track;
-class gcellgrid;
-
-// layer_def.h
-class layer;
-class via;
 
 
 #endif
